ff5bb669e50012c62db8f1d871f4b6e20f8e5706
noir <148879011+9noire@users.noreply.github.com>
2026-02-16 08:21:13 +0700

    update cv

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -49,7 +49,7 @@ export default function About() {
 
         <div className="flex justify-center md:justify-start gap-4 pt-4">
           <a
-            href="https://drive.google.com/file/d/1rzgOs5GhAO8XF33_UTTFiWIXFrhh2ZSF/view?usp=sharing"
+            href="https://drive.google.com/file/d/1euKYRDe6n_6GkP8zl_TsBo7LQ8yXXgEk/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
